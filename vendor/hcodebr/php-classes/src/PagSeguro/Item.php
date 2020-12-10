c82be5afbfaac0021e3bf15f96711c63e9fed7d2
Lucas Brito de Lima <lucasbritocientista@gmail.com>
2020-12-09 21:09:07 -0300

    Classe Item

--- vendor/hcodebr/php-classes/src/PagSeguro/Item.php
+++ vendor/hcodebr/php-classes/src/PagSeguro/Item.php
@@ -2,10 +2,61 @@
 
 namespace Hcode\PagSeguro;
 
-class Item{
+use Exception;
+use DOMElement;
+use DOMDocument;
+
+class Item
+{
 
     private $id;
     private $description;
     private $amount;
     private $quantity;
+
+    public function __construct(int $id, string $description, float $amount, int $quantity)
+    {
+        if (!$id || !$id > 0) {
+            throw new Exception("informe o ID do item.");
+        }
+
+        if (!$description) {
+            throw new Exception("informe a descrição do item.");
+        }
+
+        if (!$amount || !$amount > 0) {
+            throw new Exception("informe o valor total do item.");
+        }
+
+        if (!$quantity || !$quantity > 0) {
+            throw new Exception("informe a quantidade do item.");
+        }
+
+        $this->id = $id;
+        $this->description = $description;
+        $this->amount = $amount;
+        $this->quantity = $quantity;
+    }
+
+    public function getDOMElement(): DOMElement
+    {
+        $dom = new DOMDocument();
+
+        $item = $dom->createElement("item");
+        $item = $dom->appendChild($item);
+
+        $amount = $dom->createElement("amount", number_format($this->amount, 2, ".", ""));
+        $amount = $item->appendChild($amount);
+
+        $id = $dom->createElement("id", $this->id);
+        $id = $item->appendChild($id);
+
+        $quantity = $dom->createElement("quantity", $this->quantity);
+        $quantity = $item->appendChild($quantity);
+
+        $description = $dom->createElement("description", $this->description);
+        $description = $item->appendChild($description);
+
+        return $item;
+    }
 }
